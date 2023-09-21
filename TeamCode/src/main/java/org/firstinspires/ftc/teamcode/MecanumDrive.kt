8515337664db6bcc1b31348276202240703a18fb
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2023-09-21 09:02:41 -0400

    added MecanumDrive.kt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.kt
@@ -0,0 +1,111 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareDevice
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.util.ElapsedTime
+import com.qualcomm.robotcore.util.Range
+
+inline operator fun <reified T : HardwareDevice> HardwareMap.invoke(deviceName: String): T =
+    this.get(T::class.java, deviceName)
+
+/*
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+@TeleOp(name = "Mecanum Drive")
+//@Disabled
+class MecanumDrive : LinearOpMode() {
+    // Declare OpMode members.
+    private val runtime = ElapsedTime()
+    override fun runOpMode() {
+        telemetry.addData("Status", "Initialized")
+        telemetry.update()
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        val frDrive: DcMotor = hardwareMap("frMotor")
+        val flDrive: DcMotor = hardwareMap("flMotor")
+        val brDrive: DcMotor = hardwareMap("brMotor")
+        val blDrive: DcMotor = hardwareMap("blMotor")
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        flDrive.direction = DcMotorSimple.Direction.REVERSE
+        blDrive.direction = DcMotorSimple.Direction.REVERSE
+        frDrive.direction = DcMotorSimple.Direction.FORWARD
+        brDrive.direction = DcMotorSimple.Direction.FORWARD
+
+        flDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        blDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        frDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        brDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart()
+        runtime.reset()
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            val drive = -gamepad1.left_stick_y.toDouble()
+            val turn = gamepad1.right_stick_x.toDouble()
+            val leftPower = Range.clip(drive + turn, -1.0, 1.0)
+            val rightPower = Range.clip(drive - turn, -1.0, 1.0)
+
+            // Send calculated power to wheels
+            flDrive.power = leftPower
+            frDrive.power = rightPower
+            blDrive.power = leftPower
+            brDrive.power = rightPower
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: $runtime")
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower)
+            telemetry.update()
+        }
+    }
+}
